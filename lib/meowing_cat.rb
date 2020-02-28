1d2327f038abb556e3144f73190fcc9e5b6f3316
Simo Khouili <m.simokhouili@gmail.com>
2020-02-28 03:28:31 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+require 'pry'
+class Cat
+
+  # attr_accessor :name
+  attr_accessor :name
+
+def meow
+  puts "meow!"
+end
+
+  # def name= (name)
+  # @name = name
+  # end
+  #
+  # def name
+  #   name
+  #   maru.name = "maru"
+  # end
+end
