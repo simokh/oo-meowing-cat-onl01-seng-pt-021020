adaace352ede052e12f18b6b863721694957cf93
Simo Khouili <m.simokhouili@gmail.com>
2020-02-28 03:20:17 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -3,8 +3,8 @@ class Cat
 
   # attr_accessor :name
   attr_accessor :name
-  maru= Cat.new
-  maru.name = "maru"
+  # maru= Cat.new
+  # maru.name = "maru"
 
   def name= (name)
   @name = maru.name
